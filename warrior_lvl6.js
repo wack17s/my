1b131944f5cabe1eddd5a7a893afe628a1c1a232
Philip <keroone17@gmail.com>
2016-08-08 17:38:36 +0300

    Update warrior_lvl6.js

--- warrior_lvl6.js
+++ warrior_lvl6.js
@@ -2,26 +2,30 @@ class Player {
   
   constructor () {
     this._health = 20
-    this._direction = 'backward'    
+    this._direction = 'backward'
     this.go = (warrior) => {
       if (warrior.feel(this._direction).isCaptive()) {
         warrior.rescue(this._direction)
         this._health = warrior.health()
+        
         return
       }
       if (!warrior.feel(this._direction).isEmpty() && !warrior.feel(this._direction).isWall()) {
         warrior.attack(this._direction)
         this._health = warrior.health()
+        
         return
       }
       if (warrior.health() < this._health && warrior.feel().isEmpty() && warrior.health() < 13) {
-        warrior.walk((this._direction == 'backward') ? 'forward' : 'backward')
+        warrior.walk((this._direction === 'backward') ? 'forward' : 'backward')
         this._health = warrior.health()
+        
         return
       }      
-      if (warrior.health() < 17 && warrior.health() >=  this._health) { 
+      if (warrior.health() < 17 && warrior.health() >= this._health) { 
         warrior.rest()
         this._health = warrior.health()
+        
         return
       } 
       warrior.walk(this._direction) 
@@ -34,6 +38,7 @@ class Player {
       warrior.pivot(this._direction)
       this._direction = (this._direction === 'backward') ? 'forward' : 'backward'
       return
+      
     }
     this.go(warrior)
   }
